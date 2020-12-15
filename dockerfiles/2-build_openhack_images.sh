503ffd2acd8e3f99d45be56b43009716988f5a93
Julien LAFOURCADE <julien.lafourcade@total.com>
2020-12-15 11:56:22 +0100

    Déplacement des Dockerfiles au plus près du code parce que c'est Didier qui l'a dit

--- dockerfiles/2-build_openhack_images.sh
+++ dockerfiles/2-build_openhack_images.sh
@@ -1,12 +1,12 @@
 #docker build -t registrytzs2899.azurecr.io/poi:0.1 Dockerfile_3_POI 
 
 # Build Dockerfile_0_tripviewer :
-docker build -t registrytzs2899.azurecr.io/tripviewer:0.0.1 -f Dockerfile_0_tripviewer ../src/tripviewer
+docker build -t registrytzs2899.azurecr.io/tripviewer:0.0.1 -f Dockerfile_0_tripviewer .
 # Build Dockerfile_1_UserJava 
-docker build -t registrytzs2899.azurecr.io/UserJava:0.0.1  -f Dockerfile_1_UserJava ../src/user-java
+docker build -t registrytzs2899.azurecr.io/UserJava:0.0.1  -f Dockerfile_1_UserJava .
 # Build Dockerfile_2_UserProfile 
-docker build -t registrytzs2899.azurecr.io/UserProfile:0.0.1 -f Dockerfile_2_UserProfile ../src/userprofile
+docker build -t registrytzs2899.azurecr.io/UserProfile:0.0.1 -f Dockerfile_2_UserProfile .
 # Build Dockerfile_3_POI
-docker build -t registrytzs2899.azurecr.io/poi:0.0.1 -f Dockerfile_3_POI ../src/poi
+docker build -t registrytzs2899.azurecr.io/poi:0.0.1 -f Dockerfile_3_POI .
 # Build Dockerfile_4_trips 
-docker build -t registrytzs2899.azurecr.io/trips:0.0.1  -f Dockerfile_4_trips ../src/trips
+docker build -t registrytzs2899.azurecr.io/trips:0.0.1  -f Dockerfile_4_trips .
